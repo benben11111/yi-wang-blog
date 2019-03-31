b5599daafbf3e804c6d7e4d20e7a6753d23087c1
benben11111 <wangyi.benben@gmail.com>
2019-03-31 16:15:34 -0400

    update database url

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/personal-blog", {
+mongoose.connect("mongodb://127.0.0.1:27017/personal-blog", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
+
+// mongoose.connect("mongodb://localhost/personal-blog", {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// });
