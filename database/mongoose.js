bf419eaa6dee70bf4b426dc0c8dbd1b82a7e58a9
benben11111 <wangyi.benben@gmail.com>
2019-03-31 17:01:05 -0400

    updated mongodb route

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/personal-blog", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  "mongodb+srv://yi-wang-personal-blog:yiwangpersonalblog@cluster0-a6iyn.mongodb.net/test?retryWrites=true",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // mongoose.connect("mongodb://localhost/personal-blog", {
 //   useNewUrlParser: true,
