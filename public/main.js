44574594758f33c3b331967292018eadedfeb9ba
benben11111 <wangyi.benben@gmail.com>
2019-04-27 22:52:44 -0400

    updated UI

--- public/main.js
+++ public/main.js
@@ -1,52 +1,6 @@
-$(document).ready(function() {
-  // Position 630 for Deloitte
-  $(window).scroll(function() {
-    let position = $(this).scrollTop();
-    console.log(position);
-    if (position >= 150) {
-      $(".deloitte-logo").addClass("show-deloitte-logo");
-      $(".deloitte-text").addClass("show-deloitte-text");
-    } else {
-      $(".deloitte-logo").removeClass("show-deloitte-logo");
-      $(".deloitte-text").removeClass("show-deloitte-text");
-    }
-  });
-  // Position 980 for UN
-  $(window).scroll(function() {
-    let position = $(this).scrollTop();
-    if (position >= 600) {
-      $(".un-logo").addClass("show-un-logo");
-      $(".un-text").addClass("show-un-text");
-    } else {
-      $(".un-logo").removeClass("show-un-logo");
-      $(".un-text").removeClass("show-un-text");
-    }
-  });
-  // Position 1245 for RBC
-  $(window).scroll(function() {
-    let position = $(this).scrollTop();
-    if (position >= 790) {
-      $(".rbc-logo").addClass("show-rbc-logo");
-      $(".rbc-text").addClass("show-rbc-text");
-    } else {
-      $(".rbc-logo").removeClass("show-rbc-logo");
-      $(".rbc-text").removeClass("show-rbc-text");
-    }
-  });
-  // Position 1590 for PwC
-  $(window).scroll(function() {
-    let position = $(this).scrollTop();
-    if (position >= 1150) {
-      $(".pwc-logo").addClass("show-pwc-logo");
-      $(".pwc-text").addClass("show-pwc-text");
-    } else {
-      $(".pwc-logo").removeClass("show-pwc-logo");
-      $(".pwc-text").removeClass("show-pwc-text");
-    }
-  });
-
+$(document).ready(function () {
   // Gallery
-  $(".gallery-category").click(function() {
+  $(".gallery-category").click(function () {
     let value = $(this).attr("data-filter");
     if (value === "all") {
       $(".filter").show(300);
@@ -98,7 +52,10 @@ $(window).on("load", () => {
   // Penn Address
   const pennAddress = "University of Pennsylvania, Philadelphia, PA 19104";
   // The location of Penn
-  const pennLocation = { lat: 39.952217, lng: -75.193214 };
+  const pennLocation = {
+    lat: 39.952217,
+    lng: -75.193214
+  };
   // The map, centered at Penn
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
@@ -139,18 +96,158 @@ $(() => {
 });
 
 /*-------------------------------
+  Navigation Bar for Blogs Page 
+---------------------------------*/
+
+$(() => {
+  $(window).scroll(() => {
+    if ($(window).scrollTop() > 50) {
+      // Add white-navbar class
+      $("nav.nav-blogs").addClass("black-navbar");
+      // Show back to home button
+      $("#back-to-home").fadeIn();
+    } else {
+      // Remove white-navbar class
+      $("nav.nav-blogs").removeClass("black-navbar");
+      $("#back-to-home").fadeOut();
+    }
+  });
+});
+
+
+
+/*-------------------------------
             Smooth Scroll
 ---------------------------------*/
 
 $(() => {
-  $("a.smooth-scroll").click(function(e) {
+  $("a.smooth-scroll").click(function (e) {
     e.preventDefault();
     const sectionId = $(this).attr("href");
-    $("html, body").animate(
-      {
+    $("html, body").animate({
         scrollTop: $(sectionId).offset().top - 64
       },
       1200
     );
   });
 });
+
+/*----------------------------------------------
+            Smooth Scroll for Blogs
+-----------------------------------------------*/
+
+$(() => {
+  $("a.smooth-scroll-blogs").click(function (e) {
+    e.preventDefault();
+    const sectionId = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(sectionId).offset().top - 200
+      },
+      1200
+    );
+  });
+});
+
+
+/*-------------------------------
+            Responsive Tabs
+---------------------------------*/
+$("#experience-tabs").responsiveTabs({
+  animation: "fade",
+  startCollapsed: 'accordion'
+});
+
+
+// // Position 630 for Deloitt
+
+
+// // Position 980 for UN
+// $(window).scroll(function() {
+//   let position = $(this).scrollTop();
+//   if (position >= 600) {
+//     $(".un-logo").addClass("show-un-logo");
+//     $(".un-text").addClass("show-un-text");
+//   } else {
+//     $(".un-logo").removeClass("show-un-logo");
+//     $(".un-text").removeClass("show-un-text");
+//   }
+// });
+// // Position 1245 for RBC
+// $(window).scroll(function() {
+//   let position = $(this).scrollTop();
+//   if (position >= 790) {
+//     $(".rbc-logo").addClass("show-rbc-logo");
+//     $(".rbc-text").addClass("show-rbc-text");
+//   } else {
+//     $(".rbc-logo").removeClass("show-rbc-logo");
+//     $(".rbc-text").removeClass("show-rbc-text");
+//   }
+// });
+// // Position 1590 for PwC
+// $(window).scroll(function() {
+//   let position = $(this).scrollTop();
+//   if (position >= 1150) {
+//     $(".pwc-logo").addClass("show-pwc-logo");
+//     $(".pwc-text").addClass("show-pwc-text");
+//   } else {
+//     $(".pwc-logo").removeClass("show-pwc-logo");
+//     $(".pwc-text").removeClass("show-pwc-text");
+//   }
+// });
+
+/*-------------------------------
+            Add New Blog
+---------------------------------*/
+
+$(document).ready(() => {
+  $("#submit-button").click(() => {
+    const blogTitle = $("#blogTitle").val();
+    const blogContent = CKEDITOR.instances.blogContent.getData();
+    //console.log(`${blogTitle}: ${blogContent}`);
+
+    const blog = {
+      blogTitle,
+      blogContent
+    }
+
+    $.post("addNewBlog", {
+      blog
+    }).then(newlyAddedBlog => {
+      $("#blogTitle").val("");
+      CKEDITOR.instances.blogContent.setData("");
+    })
+
+    window.location = "/allBlogs";
+
+  });
+});
+
+// /*-------------------------------
+//             Edit Blog
+// ---------------------------------*/
+// $(document).ready(() => {
+
+//   const blogContent = $("#latestVersion").val();
+//   CKEDITOR.instances.blogContent1.setData(blogContent);
+
+//   $("#edit-blog-submit-button").click(() => {
+
+//     const title = $("#blogTitle1").val();
+//     const content = CKEDITOR.instances.blogContent1.getData();
+
+//     const blog = {
+//       title,
+//       content
+//     }
+
+//     $.ajax({
+//       url: "/allBlogs/<%= targetBlog._id %>",
+//       type: "PUT",
+//       data: blog
+//     })
+
+
+//     window.location = "/allBlogs/<%= targetBlog._id %>";
+
+//   });
+// });
